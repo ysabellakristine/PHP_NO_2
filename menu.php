5e47bf70c5f4d22b218e55cca5e18c7adbc93af0
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-02 17:26:45 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,9 @@
 <?php
 $menu_item = [
-    'Mushroom Burger' => ['price' => 47.5, 'description' => 'Best burger'],
-    'Cheese Burger'  => ['price' => 45, 'description' => 'Burger with cheese'],
-    'Normal Burger'  => ['price' => 40, 'description' => 'Plain burger for plain people'],
-    'Hawaiian Burger'  => ['price' => 70,  'description' => 'For the ones who like pineapple pizza'],
-    'Borgor'  => ['price' => 30, 'description' => 'If you buy this you have dyslexia'],
-    'Surprise drink'  => ['price' => 10,  'description' => 'Random Drink: coke, sprite, pepsi'],
+    'Example Borgor' => ['price' => 47.5, 'description' => 'Burger'],
+    'Plain fries'  => ['price' => 45, 'description' => 'Para sa boring na tao'],
+    'Extra Big fries'  => ['price' => 40, 'description' => 'Best size of fries'],
+    'Mahal Borgor'  => ['price' => 70,  'description' => 'Burger pero mahal'],
+    'Mystery Steak'  => ['price' => 10,  'description' => 'Pati ako hindi ko alam anong karne nito'],
 ];
 ?>
